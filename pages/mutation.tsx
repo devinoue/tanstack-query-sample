ce048d45fa1f8740ae68c7cb24f4ceee4930955e
Masaharu Inoue <itisagoodcat@gmail.com>
2022-12-09 02:08:52 +0900

    更新部分説明追加

--- pages/mutation.tsx
+++ pages/mutation.tsx
@@ -6,6 +6,12 @@ type Props = {
   id: number;
 };
 
+type GetTodoResponse = {
+  completed: boolean;
+  id: number;
+  title: string;
+  userId: number;
+};
 const getTodos = async ({ id }: Props) => {
   console.log('getTodo start');
   const res = await axios.get(
@@ -28,10 +34,29 @@ type PutTodoRequest = {
   userId: number;
 };
 export default function Mutation() {
+  const [id, setId] = useState(1);
   // Queries
 
-  const [id, setId] = useState(1);
-  const { data, isError, error, isLoading } = useQuery({
+  // Axiosがエラー時に投げるもの一覧。
+  // https://axios-http.com/docs/handling_errors
+
+  // useQueryにジェネリックを提供する場合２つの悪い理由がある。
+  // 1. 2つしか使わないので他の機能が使われず、不都合が発生する可能性がある
+  //３つ目のジェネリクスはselectで返される値(APIから取ってきたデータを変形修正した後のデータ)の型が期待されるのだけど、
+  // これをちゃんと入れないエラーになる
+  // 2. select を使っている中でエラーが発生するとAxiosErrorではなくErrorが飛ぶ。AxiosErrorを使わないといけないとき以外はErrorでいいと思う
+  // https://stackoverflow.com/questions/72321623/whats-the-type-of-react-querys-error-and-how-to-handle-different-cases
+
+  // なので公式ガイドでは基本的にジェネリクスは不要、別の箇所で型付けすればいい、という
+  // https://tkdodo.eu/blog/react-query-and-type-script
+  // <
+  //   GetTodoResponse,
+  //   AxiosError<{ message: string }>
+  // >
+  // だがどう考えてもinstanceofは漏れが発生する。使った方がいいと思う
+  // https://tanstack.com/query/v4/docs/typescript#typing-the-error-field
+
+  const { data, isError, error, isLoading } = useQuery<GetTodoResponse, Error>({
     queryKey: ['todos', id],
     queryFn: () => getTodos({ id }),
   });
@@ -40,7 +65,7 @@ export default function Mutation() {
   const queryClient = useQueryClient();
 
   // Mutations
-  // 型引数はなくてもいい
+  // 型引数はなくてもいいがErrorがunknownになるので、そこだけは書き換える。
   // 型引数はTData、TError、TVariablesの順番、
   // mutationFn: (variables: TVariables) => Promise<TData>の関係にある。
   // なので、mutationFnは必ずreturnを忘れずに。Promise<void>であってもreturnがないとエラーになる
@@ -49,12 +74,12 @@ export default function Mutation() {
   const mutation = useMutation<PutTodoRequest, Error, PutTodoRequest>({
     mutationFn: (variables: PutTodoRequest) => {
       // mutation.mutateからの値を確認する
-      console.log("mutation.mutateからの値")
-      console.log(variables)
-      return putTodos({ id: variables.id })
+      console.log('mutation.mutateからの値');
+      console.log(variables);
+      return putTodos({ id: variables.id });
     },
     onSuccess: (data) => {
-      console.log("レスポンスの値")
+      console.log('レスポンスの値');
       console.log(data);
       // クエリをstale としてマークされる=古いと判断して再取得できるように
       // つまりまたgetTodoが動作する
@@ -67,9 +92,12 @@ export default function Mutation() {
 
   if (isLoading) return <>LOADING</>;
   if (isError) {
-    if (error instanceof Error) {
-      return <span>{error.message}</span>;
-    }
+    // useQueryでErrorを定義しておくことの利点として、instanceofとか使わんで済むようになる
+    return <span>{error.message}</span>;
+
+    // どうしても必要なら以下のようにすればいい
+    // if (error instanceof Error) return <span>{error.message}</span>;
+    // else return <span>未知のエラーが発生しました</span>;
   }
 
   return (
